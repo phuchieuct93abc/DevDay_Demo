f838807372cf684476f7d21df1c64599d0cc8883
hieu <phuchieuct93@gmail.com>
2018-03-29 22:10:59 +0700

    Update

--- app/login/login.component.ts
+++ app/login/login.component.ts
@@ -1,26 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import * as firebase from "nativescript-plugin-firebase"
-import {  Router } from '@angular/router';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: "./login/login.component.html",
   styleUrls: ['./login/login.component.css']
 })
 export class LoginComponent implements OnInit {
-  
   constructor(private router: Router) {
-    //this.email="user1@gmail.com";
-    //this.password="123456"
-   }
+    //this.email = "user1@gmail.com";
+    //this.password = "123456"
+  }
 
-  ngOnInit() { 
-    
+  ngOnInit() {
+
   }
   login() {
    
   }
-  navigateToChat(){
-    this.router.navigateByUrl("chat")
-  }
-  
+
+
 }
